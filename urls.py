5de29c313870d0134f165237fc884615b673a007
root <root@show-01.(none)>
2013-03-15 17:19:00 +0800

    Add param name for url

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('monitoring.views',
     url(r'^phosts/$', 'phosts', name='all_phosts'),
-    url(r'^phosts/(\w+)/$', 'phosts', name='single_phost'),
-    url(r'^phosts/(\w+)/get_current/(\w+)/$', 'get_current', name='get_current'),
-    url(r'^phosts/(\w+)/get_average/(\w+)/$', 'get_average', name='get_average'),
+    url(r'^phosts/(?P<phost_id>\w+)/$', 'phosts', name='single_phost'),
+    url(r'^phosts/(?P<phost_id>\w+)/get_current/(?P<name>\w+)/$', 'get_current', name='get_current'),
+    url(r'^phosts/(?P<phost_id>\w+)/get_average/(?P<name>\w+)/$', 'get_average', name='get_average'),
 )
